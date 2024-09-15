fbf24e8472383ed4c7ac896e31cb2c46e739c774
pylxi <laramontu@gmail.com>
2024-09-15 11:25:33 +0200

    Final javascript, current location and select working

--- src/script.js
+++ src/script.js
@@ -6,6 +6,10 @@ let cities = [
   ["sydney-date", "sydney-time", "Australia/Sydney"],
 ];
 
+let citiesContainer = document.querySelector("#cities");
+
+let selectElement = document.querySelector("#timezone");
+
 // Function to update time for a specific city
 function updateTime(city) {
   let cityDate = city[0];
@@ -30,6 +34,60 @@ function updateAllTimes() {
   }
 }
 
+function updateSelectedCity(event) {
+  let cityTimeZone = event.target.value;
+
+  if (cityTimeZone === "current-location") {
+    // Get the guessed timezone from moment.js
+    let timezone = moment.tz.guess();
+    // Extract the city name from the guessed timezone
+    let cityName = timezone.split("/")[1].replace("_", " ");
+
+    citiesContainer.innerHTML = `
+      <div class="city" id="current-location">
+            <div class="info-left">
+              <h2>${cityName}</h2>
+              <div class="date" id="current-location-date">August 15th 2022</div>
+            </div>
+
+            <div class="time" id="current-location-time">1:48:15 AM</div>
+      </div>
+    `;
+    updateTime(["current-location-date", "current-location-time", timezone]);
+  }
+
+  if (cityTimeZone === "london") {
+    citiesContainer.innerHTML = `
+      <div class="city" id="london">
+            <div class="info-left">
+              <h2>London 🇬🇧</h2>
+              <div class="date" id="london-date">August 15th 2022</div>
+            </div>
+
+            <div class="time" id="london-time">1:48:15 AM</div>
+      </div>
+    `;
+
+    updateTime(["london-date", "london-time", "Europe/London"]);
+  }
+
+  if (cityTimeZone === "new-york") {
+    citiesContainer.innerHTML = `
+      <div class="city" id="new-york">
+            <div class="info-left">
+              <h2>New York 🇺🇸</h2>
+              <div class="date" id="new-york-date">August 15th 2022</div>
+            </div>
+
+            <div class="time" id="new-york-time">1:48:15 AM</div>
+      </div>
+    `;
+    updateTime(["new-york-date", "new-york-time", "America/New_York"]);
+  }
+}
+
 updateAllTimes();
 
 setInterval(updateAllTimes, 1000);
+
+selectElement.addEventListener("change", updateSelectedCity);
